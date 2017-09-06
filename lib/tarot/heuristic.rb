e07beb59dc8334af7550129146a83fd31382670c
Philihp Busby <philihp@gmail.com>
2017-09-06 03:20:48 -0700

    add naive scoring

--- lib/tarot/heuristic.rb
+++ lib/tarot/heuristic.rb
@@ -13,9 +13,33 @@ class Heuristic
     end
   end
 
+  # TODO: This is naive right now, doesn't even look at chunks of one type
   def score_tableau(tableau:)
-    # TODO: make this actually mean something
-    tableau.land.size
+    terrains = {
+      forest: { size: 0, crowns: 0 },
+      lakes: { size: 0, crowns: 0 },
+      wheat: { size: 0, crowns: 0 },
+      gardens: { size: 0, crowns: 0 },
+      mines: { size: 0, crowns: 0 },
+      village: { size: 0, crowns: 0 },
+    }
+    tableau.land.each do |tile|
+      score_tile(terrains: terrains, tileside: tile[:l])
+      score_tile(terrains: terrains, tileside: tile[:r])
+    end
+    score = 0
+    terrains.each do |k,v|
+      score += v[:size] * v[:crowns]
+    end
+    score
+  end
+
+private
+
+  def score_tile(terrains:, tileside:)
+    terrain = tileside[:terrain]
+    terrains[terrain][:size] += 1
+    terrains[terrain][:crowns] += tileside[:crowns]
   end
 
 end
